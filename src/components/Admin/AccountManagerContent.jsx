da84643a20533d9e7bd2b44e8553b9264da9ddc9
Minh Lo Do <minhldhe130866@gmail.com>
2021-10-15 03:31:35 +0700

    add pagination, filter, search of table

--- src/components/Admin/AccountManagerContent.jsx
+++ src/components/Admin/AccountManagerContent.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import CalendarEmb from "../Admin/Calendar.jsx";
 import Schedule from "../Admin/Schedule.jsx";
+import Table, { SelectColumnFilter } from "../UI/Table.jsx";
 import AccountModal from "./AccountModal.jsx";
 
 export default function AccountManagerContent() {
@@ -9,7 +10,128 @@ export default function AccountManagerContent() {
     setType(e.currentTarget.value);
     console.log(type);
   };
+  const getData = () => {
+    const data = [
+      {
+        name: "Jane Cooper",
+        email: "jane.cooper@example.com",
+        title: "Regional Paradigm Technician",
+        department: "Optimization",
+        status: "Active",
+        role: "Admin",
+        imgUrl:
+          "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+        action: (
+          <div>
+            <button
+              type="button"
+              class="btn btn-primary"
+              data-bs-toggle="modal"
+              data-bs-target="#ViewModal"
+            >
+              <i class="far fa-eye"></i>
+            </button>
+            <button
+              type="button"
+              class="btn btn-success"
+              data-bs-toggle="modal"
+              data-bs-target="#ViewModal"
+            >
+              <i class="fas fa-user-edit"></i>
+            </button>
+            <button
+              type="button"
+              class="btn btn-danger"
+              data-bs-toggle="modal"
+              data-bs-target="#ViewModal"
+            >
+              <i class="far fa-minus-square"></i>
+            </button>
+          </div>
+        ),
+      },
+      {
+        name: "Cody Fisher",
+        email: "cody.fisher@example.com",
+        title: "Product Directives Officer",
+        department: "Intranet",
+        status: "Active",
+        role: "Owner",
+        imgUrl:
+          "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+      },
+      {
+        name: "Esther Howard",
+        email: "esther.howard@example.com",
+        title: "Forward Response Developer",
+        department: "Directives",
+        status: "Active",
+        role: "Member",
+        imgUrl:
+          "https://images.unsplash.com/photo-1520813792240-56fc4a3765a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+      },
+      {
+        name: "Jenny Wilson",
+        email: "jenny.wilson@example.com",
+        title: "Central Security Manager",
+        department: "Program",
+        status: "Active",
+        role: "Member",
+        imgUrl:
+          "https://images.unsplash.com/photo-1498551172505-8ee7ad69f235?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+      },
+      {
+        name: "Kristin Watson",
+        email: "kristin.watson@example.com",
+        title: "Lean Implementation Liaison",
+        department: "Mobility",
+        status: "Active",
+        role: "Admin",
+        imgUrl:
+          "https://images.unsplash.com/photo-1532417344469-368f9ae6d187?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+      },
+      {
+        name: "Cameron Williamson",
+        email: "cameron.williamson@example.com",
+        title: "Internal Applications Engineer",
+        department: "Security",
+        status: "Active",
+        role: "Member",
+        imgUrl:
+          "https://images.unsplash.com/photo-1566492031773-4f4e44671857?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+      },
+    ];
+    return [...data, ...data, ...data];
+  };
 
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "Name",
+        accessor: "name",
+      },
+      {
+        Header: "Title",
+        accessor: "title",
+      },
+      {
+        Header: "Status",
+        accessor: "status",
+      },
+      {
+        Header: "Role",
+        accessor: "role",
+        Filter: SelectColumnFilter,
+        filter: "includes",
+      },
+      {
+        Header: "Action",
+        accessor: "action",
+      },
+    ],
+    []
+  );
+  const data = React.useMemo(() => getData(), []);
   return (
     <div class="container-fluid px-4">
       <div className="row">
@@ -25,8 +147,9 @@ export default function AccountManagerContent() {
           >
             <span class="fa fa-plus-square"></span>
           </button>
+          <Table columns={columns} data={data} />
 
-          <table id="account-table" class="table table-striped">
+          {/* <table id="account-table" class="table table-striped">
             <thead>
               <tr>
                 <th>Name</th>
@@ -172,7 +295,7 @@ export default function AccountManagerContent() {
                 </td>
               </tr>
             </tbody>
-          </table>
+          </table> */}
         </div>
 
         {/* View Modal */}
---
+++ src/components/UI/Table.jsx
@@ -0,0 +1,267 @@
+import React from "react";
+import {
+  ChevronDoubleLeftIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  ChevronDoubleRightIcon,
+} from "@heroicons/react/solid";
+
+import {
+  useTable,
+  useGlobalFilter,
+  useAsyncDebounce,
+  useFilters,
+  useSortBy,
+  usePagination,
+} from "react-table";
+
+export function SelectColumnFilter({
+  column: { filterValue, setFilter, preFilteredRows, id },
+}) {
+  // Calculate the options for filtering
+  // using the preFilteredRows
+  const options = React.useMemo(() => {
+    const options = new Set();
+    preFilteredRows.forEach((row) => {
+      options.add(row.values[id]);
+    });
+    return [...options.values()];
+  }, [id, preFilteredRows]);
+
+  return (
+    <select
+      name={id}
+      id={id}
+      value={filterValue}
+      onChange={(e) => {
+        setFilter(e.target.value || undefined);
+      }}
+    >
+      <option value="">All</option>
+      {options.map((option, i) => (
+        <option key={i} value={option}>
+          {option}
+        </option>
+      ))}
+    </select>
+  );
+}
+
+function GlobalFilter({
+  preGlobalFilteredRows,
+  globalFilter,
+  setGlobalFilter,
+}) {
+  const count = preGlobalFilteredRows.length;
+  const [value, setValue] = React.useState(globalFilter);
+  const onChange = useAsyncDebounce((value) => {
+    setGlobalFilter(value || undefined);
+  }, 200);
+
+  return (
+    <span>
+      Search:{" "}
+      <input
+        value={value || ""}
+        onChange={(e) => {
+          setValue(e.target.value);
+          onChange(e.target.value);
+        }}
+        placeholder={`${count} records...`}
+      />
+    </span>
+  );
+}
+
+function Table({ columns, data }) {
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    state,
+    preGlobalFilteredRows,
+    setGlobalFilter,
+    page,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+  } = useTable(
+    {
+      columns,
+      data,
+    },
+    useFilters,
+    useGlobalFilter,
+    useSortBy,
+    usePagination
+  );
+
+  return (
+    <>
+      <br />
+      <GlobalFilter
+        preGlobalFilteredRows={preGlobalFilteredRows}
+        globalFilter={state.globalFilter}
+        setGlobalFilter={setGlobalFilter}
+      />
+      {headerGroups.map((headerGroup) =>
+        headerGroup.headers.map((column) =>
+          column.Filter ? (
+            <div key={column.id}>
+              <label for={column.id}>{column.render("Header")}: </label>
+              {column.render("Filter")}
+            </div>
+          ) : null
+        )
+      )}
+      <div className="mt-2 flex flex-col">
+        <div className="-my-2 overflow-x-auto -mx-4 sm:-mx-6 lg:-mx-8">
+          <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div
+              className="shadow overflow-hidden border-b"
+              style={{ borderRadius: "20px" }}
+            >
+              <table
+                {...getTableProps()}
+                className="text-center"
+                style={{ width: "100%" }}
+              >
+                <thead className="bg-gray-50">
+                  {headerGroups.map((headerGroup) => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                      {headerGroup.headers.map((column) => (
+                        <th
+                          scope="col"
+                          className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                          {...column.getHeaderProps(
+                            column.getSortByToggleProps()
+                          )}
+                        >
+                          {column.render("Header")}
+                          <span>
+                            {column.isSorted
+                              ? column.isSortedDesc
+                                ? " ▼"
+                                : " ▲"
+                              : ""}
+                          </span>
+                        </th>
+                      ))}
+                    </tr>
+                  ))}
+                </thead>
+                <tbody
+                  {...getTableBodyProps()}
+                  className="bg-white divide-y divide-gray-200"
+                >
+                  {page.map((row, i) => {
+                    prepareRow(row);
+                    return (
+                      <tr {...row.getRowProps()}>
+                        {row.cells.map((cell) => {
+                          return (
+                            <td
+                              {...cell.getCellProps()}
+                              className="px-6 py-2 whitespace-nowrap border-bottom"
+                            >
+                              {cell.render("Cell")}
+                            </td>
+                          );
+                        })}
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="pagination">
+        <ul class="pagination justify-content-end">
+          <li
+            class={!canPreviousPage ? "page-item disabled" : "page-item"}
+            onClick={() => gotoPage(0)}
+          >
+            <a class="page-link" href="#" tabindex="-1">
+              <i class="fas fa-angle-double-left"></i>
+            </a>
+          </li>
+          <li
+            class={!canPreviousPage ? "page-item disabled" : "page-item"}
+            onClick={() => previousPage()}
+          >
+            <a class="page-link" href="#" tabindex="-1">
+              <i class="fas fa-angle-left"></i>
+            </a>
+          </li>
+
+          <li
+            class={!canNextPage ? "page-item disabled" : "page-item"}
+            onClick={() => nextPage()}
+          >
+            <a class="page-link" href="#" tabindex="-1">
+              <i class="fas fa-angle-right"></i>
+            </a>
+          </li>
+          <li
+            class={!canNextPage ? "page-item disabled" : "page-item"}
+            onClick={() => gotoPage(pageCount - 1)}
+          >
+            <a class="page-link" href="#" tabindex="-1">
+              <i class="fas fa-angle-double-right"></i>
+            </a>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#">
+              1
+            </a>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#">
+              2
+            </a>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#">
+              3
+            </a>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#">
+              Next
+            </a>
+          </li>
+        </ul>
+
+        <span>
+          Page{" "}
+          <strong>
+            {state.pageIndex + 1} of {pageOptions.length}
+          </strong>{" "}
+        </span>
+        <select
+          value={state.pageSize}
+          onChange={(e) => {
+            setPageSize(Number(e.target.value));
+          }}
+        >
+          {[5, 10, 20].map((pageSize) => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
+    </>
+  );
+}
+
+export default Table;
